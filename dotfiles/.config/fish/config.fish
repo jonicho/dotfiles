a603dd62b4dd9152d3cb3e05a56620efb7a02d42
Jonas Keller <jonas.r.keller@gmail.com>
2025-02-03 12:46:26 +0100

    [fish] Use LC_COLLATE=C for ls aliases

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias l "ls -lF"
+alias l "LC_COLLATE=C ls -lF"
 alias ll "l -a"
 alias lh "l -h"
 alias llh "ll -h"
