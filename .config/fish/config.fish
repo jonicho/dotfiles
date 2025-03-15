fa2c9ace3e883679c8b7fdbe7b421f44fb07b02d
Jonas Keller <jonas.r.keller@gmail.com>
2025-03-15 16:42:20 +0100

    [fish] Enable ssh-agent if it isn't enabled

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # activate ssh-agent if not enabled or active
+    if not systemctl --user is-enabled ssh-agent | grep -q enabled
+        or not systemctl --user is-active ssh-agent | grep -q active
+        systemctl --user enable --now ssh-agent
+    end
+
+    # export SSH_AUTH_SOCK
+    set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 end
+
 alias l "LC_COLLATE=C ls -lF"
 alias ll "l -a"
 alias lh "l -h"
