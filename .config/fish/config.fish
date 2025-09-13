4284706b9bdc2be0b34604a01b25f4168ad6087c
Jonas Keller <jonas.r.keller@gmail.com>
2025-09-13 11:50:07 +0200

    [fish] Start ssh-agent manually instead of using systemd service
    This enables usage of ssh-agent even if systemd is missing or doesn't work and avoids overwriting an externally provided ssh-agent (like using a forwarded ssh-agent)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,24 @@
-if status is-interactive
-    # activate ssh-agent if not enabled or active
-    if not systemctl --user is-enabled ssh-agent | grep -q enabled
-        or not systemctl --user is-active ssh-agent | grep -q active
-        systemctl --user enable --now ssh-agent
-    end
+function init_ssh_agent
+    set SSH_AGENT_ENV_FILE "$XDG_RUNTIME_DIR/ssh-agent.env.fish"
 
-    # export SSH_AUTH_SOCK
-    set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+    # if SSH_AUTH_SOCK is already set and valid, we're done
+    if test -S "$SSH_AUTH_SOCK"; return; end
+    # if not, try sourcing the ssh-agent env file
+    source "$SSH_AGENT_ENV_FILE" 2>/dev/null
+    # if SSH_AUTH_SOCK is now set and valid, we're done
+    if test -S "$SSH_AUTH_SOCK"; return; end
+    # if not, start ssh-agent, save output to the env file and source it
+    ssh-agent -c > "$SSH_AGENT_ENV_FILE"
+    source "$SSH_AGENT_ENV_FILE"
+    # if SSH_AUTH_SOCK is now set and valid, we're done
+    if test -S "$SSH_AUTH_SOCK"; return; end
+    # if not, something went wrong
+    echo "Failed to start ssh-agent" >&2
+    return 1
+end
+
+if status is-interactive
+    init_ssh_agent
 end
 
 alias l "LC_COLLATE=C ls -lF"
